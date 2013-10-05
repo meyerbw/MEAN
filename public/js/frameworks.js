559ae86c3e163559b9d8ec20ef08c1940c1e8b07
ebertsch <Wicked82>
2013-10-04 22:33:58 -0400

    Added Assets

--- public/js/frameworks.js
+++ public/js/frameworks.js
@@ -1,2 +1,13 @@
 //= require lib/jquery/jquery-2.0.3.js
 //= require lib/bootstrap/bootstrap.min.js
+
+//= require lib/angular/angular.js
+//= require lib/angular/angular-animate.js
+//= require lib/angular/angular-cookies.js
+//= require lib/angular/angular-loader.js
+//= require lib/angular/angular-mocks.js
+//= require lib/angular/angular-resource.js
+//= require lib/angular/angular-route.js
+//= require lib/angular/angular-sanitize.js
+//= require lib/angular/angular-scenario.js
+//= require lib/angular/angular-touch.js
