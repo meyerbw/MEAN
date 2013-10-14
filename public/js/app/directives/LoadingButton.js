d2a9a69a4980839075a391d7a5f92697d5f400bb
Ezra Bertsch <ebertsch@gmail.com>
2013-10-14 15:42:15 -0400

    Added a Loading Button Direvtive

--- public/js/app/directives/LoadingButton.js
+++ public/js/app/directives/LoadingButton.js
@@ -1,24 +1,19 @@
 angular.module('mean')
-	.directive('rmsStatefulButton', ['$compile', function ($compile) {
-		return {
-			restrict: 'A',
-			replace: false,
-			transclude: false,
-			scope: {
-				loading: '=loading',
-				clickHandler: '&ngClick'
-			},
-			link: function (scope, el, atts) {
-				atts.$watch('loading', function () {
-					if (scope.loading) {
-                        $(el).button('loading')
-					} else {
-                        $(el).button('reset')
-					}
-				});
+    .directive('loadingButton', [function () {
+        return {
+            transclude: false,
+            scope: true,
+            link: function (scope, el, atts) {
+                atts.$observe('loading', function (value) {
+                    var casedVal = value.toLowerCase();
 
-			  el.bind('click', function() {scope.clickHandler()});
+                    var isTrue = (casedVal === "true" || casedVal === "yes" || casedVal === "1" );
 
-			}
-		};
-	}]);
+                    if (isTrue)
+                        $(el).button('loading');
+                    else
+                        $(el).button('reset');
+                });
+            }
+        };
+    }]);
